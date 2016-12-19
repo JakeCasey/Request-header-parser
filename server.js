85d01e91f9b68f09f20acbfa16e16f6da72b737c
Jake Casey <acasestudy0@gmail.com>
2016-12-19 21:32:34 +0000

    final commit

--- server.js
+++ server.js
@@ -1,12 +1,63 @@
 var express = require('express');
+var ejs = require('ejs');
+var path = require('path');
+
 
 
 var app = express()
+app.set('view engine', 'ejs');
+app.use('/views', express.static(path.join(__dirname, 'views')))
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index')
+})
+
+app.get('/:date', function(req, res){
+    var date1;
+    
+    //catch unix time and modify for javascript date object.
+    if(req.params.date.match(/[a-z]/i)){
+        date1 = new Date(req.params.date);
+    }
+    else{
+        date1 = new Date(req.params.date*1000);
+    }
+   
+    console.log(date1);
+    
+    //catch nondates
+    if(isNaN(date1)){
+        res.send(JSON.stringify({"unix": null, "natural": null}));
+    }
+    
+
+    //find natural language month
+    function monthName(a){
+      var months = ['January', 'February', 'March','April','May','June','July','August','September','October','November','December'];
+      return months[a];
+    }
+    
+    //create natural language date
+    var nlDateArr = [];
+    var nlDate = date1;
+    nlDateArr.push(monthName(nlDate.getMonth()) + ' ');
+    nlDateArr.push(nlDate.getDay() + ', ')
+    nlDateArr.push(nlDate.getFullYear());
+    nlDateArr = nlDateArr.join('');
+   
+   //return obj
+    var date = {
+
+        "unix": date1.getTime(),
+        "natural": nlDateArr
+    }
+        
+    
+    
+    
+    res.send(JSON.stringify(date));
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('listening on port 8080!')
 })
