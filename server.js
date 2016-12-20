53766dcfe0e583875a4e84b4da7c36b2c1705bfe
Jake Casey <acasestudy0@gmail.com>
2016-12-20 01:27:49 +0000

    init

--- server.js
+++ server.js
@@ -12,49 +12,13 @@ app.get('/', function (req, res) {
   res.render('index')
 })
 
-app.get('/:date', function(req, res){
-    var date1;
+app.get('/api/whoami', function(req, res){
     
-    //catch unix time and modify for javascript date object.
-    if(req.params.date.match(/[a-z]/i)){
-        date1 = new Date(req.params.date);
-    }
-    else{
-        date1 = new Date(req.params.date*1000);
-    }
- 
-    
-    //catch nondates
-    if(isNaN(date1)){
-        res.send(JSON.stringify({"unix": null, "natural": null}));
-    }
-    
-
-    //find natural language month
-    function monthName(a){
-      var months = ['January', 'February', 'March','April','May','June','July','August','September','October','November','December'];
-      return months[a];
-    }
-    
-    //create natural language date
-    var nlDateArr = [];
-    var nlDate = date1;
-    nlDateArr.push(monthName(nlDate.getMonth()) + ' ');
-    nlDateArr.push(nlDate.getDay() + ', ')
-    nlDateArr.push(nlDate.getFullYear());
-    nlDateArr = nlDateArr.join('');
-   
-   //return obj
-    var date = {
-
-        "unix": date1.getTime(),
-        "natural": nlDateArr
-    }
         
     
     
     
-    res.send(JSON.stringify(date));
+    res.send('test');
 })
 
 app.listen(process.env.PORT || 80, function () {
